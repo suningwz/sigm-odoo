d249ea4ce1d7e97700aa07a5ac7fabd99ccc6fe6
murielremi <remimanantsoa@gmail.com>
2022-02-03 14:39:20 +0300

    product.template instead of product.product

--- block_deleting_used_product/models/product.py
+++ block_deleting_used_product/models/product.py
@@ -3,8 +3,8 @@
 from odoo import fields, api, models, _
 from odoo.exceptions import UserError
 
-class ProductProduct(models.Model):
-	_inherit = 'product.product'
+class ProductTemplate(models.Model):
+	_inherit = 'product.template'
 
 	def unlink(self):
 		raise UserError("deletion blocked")
