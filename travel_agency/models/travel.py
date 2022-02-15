bd033005438fb11dbbd4ed62d148f787f75f1b29
murielremi <remimanantsoa@gmail.com>
2022-02-15 21:18:13 +0300

    force firstname and lastname to uppercase

--- travel_agency/models/travel.py
+++ travel_agency/models/travel.py
@@ -706,6 +706,7 @@ class TravelOrderLine(models.Model):
 
     @api.onchange('passenger_firstname')
     def onchange_firstname(self):
+        self.passenger_firstname = self.passenger_firstname.upper()
         name = []
         if self.journey:
             name.append(self.journey)
@@ -718,6 +719,7 @@ class TravelOrderLine(models.Model):
         self.name = " - ".join(name)
 
     @api.onchange('passenger_lastname')
+        self.passenger_lastname = self.passenger_lastname.upper()
     def onchange_lastname(self):
         name = []
         if self.journey:
