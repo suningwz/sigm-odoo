f2ce6357ffc25a4400f8acba354592e4bc952cca
murielremi <remimanantsoa@gmail.com>
2022-02-17 16:59:12 +0300

    raise user error product_id.category

--- travel_agency/models/travel.py
+++ travel_agency/models/travel.py
@@ -729,7 +729,7 @@ class TravelOrderLine(models.Model):
     # --------------------------------------------------------
     @api.onchange('product_id')
     def onchange_product(self):
-        raise UserError(self.category)
+        raise UserError(self.product_id.category)
         name_elements = [self.journey, self.ticket_number, self.passenger, self.custom_descri]
 
         name_elements = [item for item in name_elements if item]
